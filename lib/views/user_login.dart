28c6c5df4c93ff0d70b19f6ba6110d905fd6cd8d
Kelwin Amaral <klw.amaral@gmail.com>
2024-09-29 11:51:23 -0300

    Login funcionando...

--- lib/views/user_login.dart
+++ lib/views/user_login.dart
@@ -1,15 +1,28 @@
+import 'package:misale/components/user_tile.dart';
+import 'package:misale/routes/app_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:misale/provider/users.dart';
 
 class UserLogin extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.amber,
-      ),
-
+        appBar: AppBar(
+          title: const Text('Loguin'),
+          actions: <Widget>[
+            IconButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed(AppRoutes.user_list);
+                },
+                icon: const Icon(Icons.add)),
+          ],
+        )
     );
+
+
+
   }
 }
+
 
